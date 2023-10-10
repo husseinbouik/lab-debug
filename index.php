6940560fda50e9c0cb9f0f92afb7721002eaaf3f
husseinbouik <husseinbouik5@gmail.com>
2023-10-10 13:56:32 +0100

    lab-debug

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <?php
-$x = 10;
-$b = 5;
-$c =$x + $b ;
+$servername = "localhost:3306";
+$username = "root";
+$password = "Hbouki.2002";
+$dbname = "arbre_competence";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=" . $dbname, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully";
+
+    $sql = "SELECT * FROM `personne`";
+    $result = $conn->query($sql);
+
+    echo "<table border='1'>";
+    echo "<tr><th>ID</th><th>Full Name</th><th>Email</th></tr>";
+
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        echo "<tr>";
+        echo "<td>" . $row['Id'] . "</td>";
+        echo "<td>" . $row['Nom'] . "</td>";
+        echo "<td>" . $row['CNE'] . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
+// phpinfo();
+
+
+
 ?>
